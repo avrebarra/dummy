2225e2d67c3a6dd3461d430fe61ffd644aa40f96
Avreghly Barra <avreghly@gmail.com>
2020-08-25 09:01:21 +0700

    init node

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-console.log('hello world 4')
-console.log('hello world 4')
-console.log('hello world 4')
-console.log('hello world 4')
-console.log('hello world 4')
-console.log('hello world 4')
-console.log('hello world asdas4')
-
-editane yan
+console.log('hello world')
